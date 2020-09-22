500662645594caf1a6e6d7f0345dc03a685a6f6a
EdieAprovita <edieveg316@gmail.com>
2020-09-22 12:41:43 -0500

    Adding styles to footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='main'>
       <Navbar />
+      <br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam facere
+      natus numquam. Ab repellat magnam nemo est ipsam nihil? Voluptatibus ea
+      ipsum doloribus atque dolorem voluptatem quam similique earum corporis.
+      <br />
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,56 +1,105 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 const Footer = () => {
   return (
-    <div className='main-footer'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-3 col-sm-6'>
-            <h4>Hola</h4>
-            <ul className='list-unstyled'>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-            </ul>
+    <FooterContainer className='main-footer'>
+      <div className='footer-middle'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-3 col-sm-6'>
+              <h4>Hola</h4>
+              <ul className='list-unstyled'>
+                <li>Hola mundo</li>
+                <li>Hola mundo</li>
+                <li>Hola mundo</li>
+                <li>Hola mundo</li>
+              </ul>
+            </div>
+            <div className='col-md-3 col-sm-6'>
+              <h4>Hola</h4>
+              <ul className='list-unstyled'>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+              </ul>
+            </div>
+            <div className='col-md-3 col-sm-6'>
+              <h4>Hola</h4>
+              <ul className='list-unstyled'>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+              </ul>
+            </div>
+            <div className='col-md-3 col-sm-6'>
+              <h4>Hola</h4>
+              <ul className='list-unstyled'>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+                <li>
+                  <a href='/'>Hola mundo</a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className='col-md-3 col-sm-6'>
-            <h4>Hola</h4>
-            <ul className='list-unstyled'>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-            </ul>
+          <div className='footer-bottom'>
+            <p className='text-xs-center'>
+              {new Date().getFullYear()} City Guide For Vegan Restaurants- All
+              Rights Reserved
+            </p>
           </div>
-          <div className='col-md-3 col-sm-6'>
-            <h4>Hola</h4>
-            <ul className='list-unstyled'>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-            </ul>
-          </div>
-          <div className='col-md-3 col-sm-6'>
-            <h4>Hola</h4>
-            <ul className='list-unstyled'>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-              <li>Hola mundo</li>
-            </ul>
-          </div>
-        </div>
-        <div className='footer-bottom'>
-          <p className='text-xs-center'>
-            {new Date().getFullYear()} City Guide For Vegan Restaurants- All
-            Rights Reserved
-          </p>
         </div>
       </div>
-    </div>
+    </FooterContainer>
   );
 };
 
 export default Footer;
+
+const FooterContainer = styled.footer`
+  .footer-middle {
+    background: var(--mainDark);
+    padding-top: 3rem;
+    color: var(--mainWhite);
+  }
+
+  .footer-bottom {
+    padding-top: 3rem;
+    padding-bottom: 2rem;
+  }
+
+  ul li a {
+    color: var(--mainGray);
+  }
+
+  ul li a:hover {
+    color: var(--mainLightGrey);
+  }
+`;
